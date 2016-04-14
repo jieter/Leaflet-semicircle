4b8bcf7ee996f4a4b49407dc14fdbd3fbc6462ec
Jan Pieter Waagmeester <jieter@jieter.nl>
2016-04-14 13:33:59 +0200

    Tabs to spaces + more strict rules

--- Semicircle.js
+++ Semicircle.js
@@ -20,77 +20,78 @@
     }
 })(function (L) {
 
-	var DEG_TO_RAD = Math.PI / 180;
+    var DEG_TO_RAD = Math.PI / 180;
 
-	// make sure 0 degrees is up (North) and convert to radians.
-	function fixAngle (angle) {
-		return (angle - 90) * DEG_TO_RAD;
-	}
+    // make sure 0 degrees is up (North) and convert to radians.
+    function fixAngle (angle) {
+        return (angle - 90) * DEG_TO_RAD;
+    }
 
-	L.Circle = L.Circle.extend({
-		options: {
-			startAngle: 0,
-			stopAngle: 359.9999
-		},
+    // rotate point [x + r, y+r] around [x, y] with `angle`.
+    function rotated (point, angle, r) {
+        return point.add(
+            L.point(Math.cos(angle), Math.sin(angle)).multiplyBy(r)
+        ).round();
+    }
 
-		startAngle: function () {
-			return fixAngle(this.options.startAngle);
-		},
-		stopAngle: function () {
-			return fixAngle(this.options.stopAngle);
-		},
+    L.Point.prototype.rotated = function (angle, r) {
+        return rotated(this, angle, r);
+    };
 
-		// rotate point x,y+r around x,y with angle.
-		rotated: function (angle, r) {
-			return this._point.add(
-				L.point(Math.cos(angle), Math.sin(angle)).multiplyBy(r)
-			).round();
-		},
 
-		setStartAngle: function (angle) {
-			this.options.startAngle = angle;
-			return this.redraw();
-		},
-		setStopAngle: function (angle) {
-			this.options.stopAngle = angle;
-			return this.redraw();
-		},
-		setDirection: function (direction, degrees) {
-			if (degrees === undefined) {
-				degrees = 10;
-			}
-			this.options.startAngle = direction - (degrees / 2);
-			this.options.stopAngle = direction + (degrees / 2);
+    L.Circle = L.Circle.extend({
+        options: {
+            startAngle: 0,
+            stopAngle: 359.9999
+        },
 
-			return this.redraw();
-		}
-	});
+        startAngle: function () { return fixAngle(this.options.startAngle); },
+        stopAngle: function () { return fixAngle(this.options.stopAngle); },
 
-	// save original getPathString function to draw a full circle.
-	var originalUpdateCircle = L.SVG.prototype._updateCircle;
+        setStartAngle: function (angle) {
+            this.options.startAngle = angle;
+            return this.redraw();
+        },
+        setStopAngle: function (angle) {
+            this.options.stopAngle = angle;
+            return this.redraw();
+        },
+        setDirection: function (direction, degrees) {
+            if (degrees === undefined) {
+                degrees = 10;
+            }
+            this.options.startAngle = direction - (degrees / 2);
+            this.options.stopAngle = direction + (degrees / 2);
 
-	L.SVG.include({
-		_updateCircle: function (layer) {
-			// If we want a circle, we use the original function
-			if (layer.options.startAngle === 0 && layer.options.stopAngle > 359) {
-				return originalUpdateCircle.call(this, layer);
-			}
+            return this.redraw();
+        }
+    });
 
-			var center = layer._point,
-				r = layer._radius;
+    // save original getPathString function to draw a full circle.
+    var originalUpdateCircle = L.SVG.prototype._updateCircle;
 
-			var start = layer.rotated(layer.startAngle(), r),
-				end = layer.rotated(layer.stopAngle(), r);
+    L.SVG.include({
+        _updateCircle: function (layer) {
+            // If we want a circle, we use the original function
+            if (layer.options.startAngle === 0 && layer.options.stopAngle > 359) {
+                return originalUpdateCircle.call(this, layer);
+            }
 
-			var largeArc = (layer.options.stopAngle - layer.options.startAngle >= 180) ? '1' : '0';
-			// move to center
-			var d = 'M' + center.x + ',' + center.y;
-			// lineTo point on circle startangle from center
-			d += 'L ' + start.x + ',' + start.y;
-			// make circle from point start - end:
-			d += 'A ' + r + ',' + r + ',0,' + largeArc + ',1,' + end.x + ',' + end.y + ' z';
+            var center = layer._point,
+                r = layer._radius;
 
-			this._setPath(layer, d);
-		}
-	})
+            var start = layer._point.rotated(layer.startAngle(), r),
+                end = layer._point.rotated(layer.stopAngle(), r);
+
+            var largeArc = (layer.options.stopAngle - layer.options.startAngle >= 180) ? '1' : '0';
+            // move to center
+            var d = 'M' + center.x + ',' + center.y;
+            // lineTo point on circle startangle from center
+            d += 'L ' + start.x + ',' + start.y;
+            // make circle from point start - end:
+            d += 'A ' + r + ',' + r + ',0,' + largeArc + ',1,' + end.x + ',' + end.y + ' z';
+
+            this._setPath(layer, d);
+        }
+    })
 });
