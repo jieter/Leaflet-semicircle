3e3161eb7dbce2f51c1758f7f94476b0e3e7d729
Jan Pieter Waagmeester <jieter@jieter.nl>
2016-12-06 14:58:46 +0100

    update tested with version in Semicircle.js

--- Semicircle.js
+++ Semicircle.js
@@ -2,7 +2,7 @@
  * Semicircle extension for L.Circle.
  * Jan Pieter Waagmeester <jieter@jieter.nl>
  *
- * This version is tested with leaflet 1.0.0-rc.1
+ * This version is tested with leaflet 1.0.2
  */
 (function (factory) {
     if (typeof define === 'function' && define.amd) {
