e2d00669803ba031c1583e29a9e5a8e8a8147949
uygardonduran <uygardonduran@gmail.com>
2018-08-28 16:53:31 +0300

    canvas fix (#29)
    Added _drawing check for canvas in the original leaflet code to prevent duplicate draws.

--- Semicircle.js
+++ Semicircle.js
@@ -166,6 +166,8 @@
                 return _updateCircleCanvas.call(this, layer);
             }
 
+            if (!this._drawing || layer._empty()) { return; }
+
             var p = layer._point,
                 ctx = this._ctx,
                 r = layer._radius,
